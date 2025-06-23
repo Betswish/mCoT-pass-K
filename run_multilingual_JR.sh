70de97b9bf44d4404c597fa29bf7c252be0eb945
Betswish <j.qi@rug.nl>
2025-06-23 17:24:27 +0200

    fix

--- run_multilingual_JR.sh
+++ run_multilingual_JR.sh
@@ -61,7 +61,7 @@ LANGUAGES_THINK=(
 # Define datasets to use
 DATASETS=(
   # # AIME combined dataset
-  "aime_combined:problem:answer"
+  "shanchen/aime_combined:problem:answer"
 
   # # GPQA dataset
   "shanchen/gpqa_diamond_mc_multilingual:problem:solution"
@@ -124,8 +124,6 @@ run_job() {
   
   # Run the inference
   {
-    # Special handling for datasets that need language config
-    if [[ "$DATASET" == "juletxara/mgsm" || "$DATASET" == "shanchen/aiw_hard_multilingual" || "$DATASET" == "shanchen/aime_2024_multilingual" || "$DATASET" == "shanchen/aime_2025_multilingual" || "$DATASET" == "aime_combined" || "$DATASET" == "shanchen/gpqa_diamond_mc_multilingual" ]]; then
     # Convert LANG to lowercase for config
     LANG_LOWER=$(echo "$LANG" | tr '[:upper:]' '[:lower:]')
     
